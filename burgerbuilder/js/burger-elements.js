787d41d673bd037adcdcbda887efec8402a38f66
nielsVoogt <niels@branchonline.nl>
2017-07-30 17:21:12 +0200

    The burgerbuilder itself

---
+++ burgerbuilder/js/burger-elements.js
@@ -0,0 +1,135 @@
+
+const toppings = {
+    cheese: {
+        cheddar: {
+            shape: 'img/shapes/cheese_1.png',
+            calories: 1
+        },
+        emmentaler: {
+            shape: 'img/shapes/cheese_1.png',
+            calories: 1
+        },
+        mozerella: {
+            shape: 'img/shapes/cheese_1.png',
+            calories: 1
+        },
+        blue: {
+            shape: 'img/shapes/cheese_1.png',
+            calories: 1
+        },
+        gouda: {
+            shape: 'img/shapes/cheese_1.png',
+            calories: 1
+        }
+    },
+    meat: {
+        chicken: {
+            shape: 'img/shapes/meat_1.png',
+            calories: 2
+        },
+        cow: {
+            shape: 'img/shapes/meat_1.png',
+            calories: 2
+        },
+        calf: {
+            shape: 'img/shapes/meat_1.png',
+            calories: 2
+        },
+        bacon: {
+            shape: 'img/shapes/meat_1.png',
+            calories: 2
+        }
+    },
+    greens: {
+        fresh_onion: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        grilled_onion: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        onion_rings: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        lettuce: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        tomato: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        pickles: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        mushrooms: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        avocado_slices: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        },
+        pineapple: {
+            shape: 'img/shapes/greens_1.png',
+            calories: 3
+        }
+    },
+    sauce: {
+        ketchup: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        mustard: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        dijon_mustard: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        mayonaise: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        belgian_mayonaise: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        mustard: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        bbq_sauce: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        hot_sauce: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        guacamole: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        smoke_sauce: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        worchester: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        aoli: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        },
+        pesto: {
+            shape: 'img/shapes/sauce_1.png',
+            calories: 4
+        }
+    }
+}
---
+++ burgerbuilder/js/burgerbuilder.js
@@ -0,0 +1,145 @@
+
+let pos;
+let burger = [];
+
+let toppingIndex  = 0,
+    insertIndex   = 0,
+    totalCalories = 0;
+
+const addToppingOptions = document.getElementsByClassName('add-topping');
+const selectToppingType = document.getElementsByClassName('toppings__type-selector');
+const toppingType = document.getElementsByClassName('toppings__type');
+const editToppingButtons = document.getElementsByClassName('options__option');
+
+/// -----------------------------------------------------------------------------------------------
+
+const listenerAssigner = function(nodelist, type, fn) {
+    for(var i = 0; i < nodelist.length; i++) {
+        nodelist[i].addEventListener('click', fn);
+    }
+};
+
+const UIController = (function() {
+
+    const btnMoveDown = '<a class="options__option" id="down">D</a>',
+            btnMoveUp = '<a class="options__option" id="up">U</a>',
+            btnRemove = '<a class="options__option" id="delete">X</a>';
+
+    const toppingTemplate = function(toppingImage, calories, category, name) {
+        return `<div
+                    class="topping"
+                    id="${toppingIndex++}"
+                    data-calories="${calories}"
+                    data-category="${category}"
+                    data-name="${name}"
+                    style="background-image: url(${toppingImage});"
+                >
+                    <div class="options">
+                        ${btnMoveUp}
+                        ${btnMoveDown}
+                        ${btnRemove}
+                    </div>
+                </div>`;
+    }
+
+    const getPositionInArray = function(arr, id) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].match(`id="${id}"`)) {
+                return pos = arr.indexOf(arr[i]);
+            }
+        }
+    }
+
+    const swapArrayElements = function(arr, indexA, indexB) {
+        let temp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = temp;
+    };
+
+    const buildBurger = function() {
+        document.getElementById('holder').innerHTML = burger.join("");
+        listenerAssigner(editToppingButtons, 'click', editTopping);
+    };
+
+    const insertTopping = function() {
+        let el = this.closest('.topping');
+        insertIndex = getPositionInArray(burger, el.id) + 1;
+    }
+
+    const removeTopping = function(el) {
+        burger.splice(getPositionInArray(burger, el.closest('.topping').id), 1);
+        totalCalories = totalCalories - el.closest('.topping').getAttribute('data-calories');
+    };
+
+    const editTopping = function() {
+        getPositionInArray(burger, this.closest('.topping').id);
+        if (this.id === 'up' && pos != 0) {
+            swapArrayElements(burger, pos, pos - 1);
+            buildBurger();
+        } else if (this.id === 'down' && pos != burger.length - 1) {
+            swapArrayElements(burger, pos, pos + 1);
+            buildBurger();
+        } else if (this.id === 'delete') {
+            removeTopping(this);
+            buildBurger();
+        }
+    };
+
+    return {
+        createToppingLinks: function(topping, type) {
+            for (var i = topping.length - 1; i >= 0; i--) {
+                let toppingName = topping[i].replace('_', ' ');
+                document.getElementById(type).insertAdjacentHTML('beforeend', `<li><a class="add-topping" type="${type}" id=${topping[i]}>${toppingName}</a></li>`)
+            }
+        },
+
+        triggerTypeMenu: function() {
+            let toppingModalOptions = document.getElementsByClassName('toppings__type');
+
+            for(let i = 0; i < toppingModalOptions.length; i++) {
+                toppingModalOptions[i].classList.remove('toppings__type--active');
+            }
+
+            document.getElementById(this.getAttribute('trigger')).classList.toggle('toppings__type--active');
+        },
+
+        addTopping: function() {
+            let  toppingName     = this.id,
+                 toppingType     = this.getAttribute('type'),
+                 toppingCalories = toppings[toppingType][toppingName].calories,
+                 toppingImage    = toppings[toppingType][toppingName].shape;
+
+            burger.splice(insertIndex, 0, toppingTemplate(toppingImage, toppingCalories, toppingType, toppingName));
+            totalCalories = totalCalories + toppingCalories;
+            buildBurger();
+        },
+    }
+
+})();
+
+/// -----------------------------------------------------------------------------------------------
+
+const BurgerController = (function() {
+
+    const setUpEventListeners = function() {
+        listenerAssigner(addToppingOptions, 'click', UIController.addTopping);
+        listenerAssigner(selectToppingType, 'click', UIController.triggerTypeMenu);
+    };
+
+    const buildToppingNavigation = function() {
+        UIController.createToppingLinks(Object.keys(toppings.cheese), 'cheese');
+        UIController.createToppingLinks(Object.keys(toppings.sauce), 'sauce');
+        UIController.createToppingLinks(Object.keys(toppings.meat), 'meat');
+        UIController.createToppingLinks(Object.keys(toppings.greens), 'greens');
+    }
+
+    return {
+        init: function() {
+            buildToppingNavigation();
+            setUpEventListeners();
+        }
+    };
+
+})();
+
+BurgerController.init();
